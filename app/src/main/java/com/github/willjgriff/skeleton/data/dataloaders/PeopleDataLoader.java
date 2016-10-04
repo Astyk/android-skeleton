ea1a8c538c0661caff1794df22d7ebe4c866c283
willjgriff <willjgriff@gmail.com>
2016-10-04 23:28:50 +0100

    Created an extra layer for individual DataLoaders.

---
+++ app/src/main/java/com/github/willjgriff/skeleton/data/dataloaders/PeopleDataLoader.java
@@ -0,0 +1,52 @@
+package com.github.willjgriff.skeleton.data.dataloaders;
+
+import android.support.annotation.NonNull;
+
+import com.github.willjgriff.skeleton.data.NetworkFetchAndUpdateList;
+import com.github.willjgriff.skeleton.data.models.Person;
+import com.github.willjgriff.skeleton.data.network.services.PeopleService;
+import com.github.willjgriff.skeleton.data.responsewrapper.NetworkResponseWrapper;
+import com.github.willjgriff.skeleton.data.responsewrapper.RealmResponseWrapper;
+import com.github.willjgriff.skeleton.data.responsewrapper.ResponseHolder;
+import com.github.willjgriff.skeleton.data.storage.fetchers.AllRealmFetcher;
+import com.github.willjgriff.skeleton.data.storage.fetchers.RealmFetcher;
+import com.github.willjgriff.skeleton.data.storage.updaters.BasicAsyncRealmUpdater;
+import com.github.willjgriff.skeleton.data.storage.updaters.RealmUpdater;
+import com.github.willjgriff.skeleton.data.storage.updaters.methods.RealmUpdateMethod;
+import com.github.willjgriff.skeleton.data.storage.updaters.methods.ReplaceListRealmUpdateMethod;
+
+import java.util.List;
+
+import io.realm.Realm;
+import rx.Observable;
+
+/**
+ * Created by Will on 04/10/2016.
+ */
+public class PeopleDataLoader {
+
+	private Realm mRealm;
+	private PeopleService mPeopleService;
+	private RealmFetcher<Person> mPeopleRealmFetcher;
+
+	public PeopleDataLoader(@NonNull Realm realm, @NonNull PeopleService peopleService) {
+		mRealm = realm;
+		mPeopleService = peopleService;
+		mPeopleRealmFetcher = new AllRealmFetcher<>(Person.class);
+	}
+
+	public Observable<ResponseHolder<List<Person>>> getPeopleFromCache() {
+		RealmResponseWrapper<Person> realmResponseWrapper = new RealmResponseWrapper<>();
+		return realmResponseWrapper.wrap(mPeopleRealmFetcher.getAsyncObservable(mRealm));
+	}
+
+	public Observable<ResponseHolder<List<Person>>> getPeopleFromNetwork(int countPeople) {
+		RealmUpdateMethod<List<Person>> realmUpdateMethod = new ReplaceListRealmUpdateMethod<>(mPeopleRealmFetcher);
+		RealmUpdater<List<Person>> realmAsyncUpdater = new BasicAsyncRealmUpdater<>(mRealm, realmUpdateMethod);
+		NetworkFetchAndUpdateList<Person> peopleNetworkFetchAndUpdateList = new NetworkFetchAndUpdateList<>(
+			mPeopleService.getPeople(countPeople), realmAsyncUpdater);
+
+		NetworkResponseWrapper<Person> networkResponseWrapper = new NetworkResponseWrapper<>();
+		return networkResponseWrapper.wrap(peopleNetworkFetchAndUpdateList.getNetworkObservable());
+	}
+}
--- app/src/main/java/com/github/willjgriff/skeleton/ui/people/data/PeopleDataManager.java
+++ app/src/main/java/com/github/willjgriff/skeleton/ui/people/data/PeopleDataManager.java
@@ -2,18 +2,10 @@ package com.github.willjgriff.skeleton.ui.people.data;
 
 import android.support.annotation.NonNull;
 
-import com.github.willjgriff.skeleton.data.NetworkFetchAndUpdateList;
+import com.github.willjgriff.skeleton.data.dataloaders.PeopleDataLoader;
 import com.github.willjgriff.skeleton.data.models.Person;
 import com.github.willjgriff.skeleton.data.network.services.PeopleService;
-import com.github.willjgriff.skeleton.data.responsewrapper.NetworkResponseWrapper;
-import com.github.willjgriff.skeleton.data.responsewrapper.RealmResponseWrapper;
 import com.github.willjgriff.skeleton.data.responsewrapper.ResponseHolder;
-import com.github.willjgriff.skeleton.data.storage.fetchers.AllRealmFetcher;
-import com.github.willjgriff.skeleton.data.storage.fetchers.RealmFetcher;
-import com.github.willjgriff.skeleton.data.storage.updaters.BasicAsyncRealmUpdater;
-import com.github.willjgriff.skeleton.data.storage.updaters.RealmUpdater;
-import com.github.willjgriff.skeleton.data.storage.updaters.methods.RealmUpdateMethod;
-import com.github.willjgriff.skeleton.data.storage.updaters.methods.ReplaceListRealmUpdateMethod;
 
 import java.util.List;
 
@@ -24,27 +16,26 @@ import rx.subjects.PublishSubject;
 /**
  * Created by Will on 06/09/2016.
  */
-// TODO: Once we're happy with the setup, abstract this so we can have
-// specific data loaders that a single data manager looks after.
+// DataManagers encapsulate all a screen's data requirements
+// DataLoaders encapsulate fetching of a specific data model
 public class PeopleDataManager {
 
 	private Realm mRealm;
-	private PeopleService mPeopleService;
-	private NetworkFetchAndUpdateList<Person> mPeopleNetworkFetchAndUpdateList;
-	private RealmFetcher<Person> mPeopleRealmFetcher;
+	private PeopleDataLoader mPeopleDataLoader;
 	private PublishSubject<Void> mNetworkFetchTrigger;
 	private boolean mNetworkDataFetched = false;
 
 	public PeopleDataManager(@NonNull Realm realm, @NonNull PeopleService peopleService) {
 		mRealm = realm;
-		mPeopleService = peopleService;
-		mPeopleRealmFetcher = new AllRealmFetcher<>(Person.class);
 		mNetworkFetchTrigger = PublishSubject.create();
+		mPeopleDataLoader = new PeopleDataLoader(realm, peopleService);
 	}
 
 	public Observable<ResponseHolder<List<Person>>> getPeopleObservable(int countPeople) {
 		// This HAS to be a merge, not a concat, to enable the network trigger to work.
-		return Observable.merge(getPeopleFromCache(), getPeopleFromNetworkTrigger(countPeople))
+		return Observable.merge(
+			mPeopleDataLoader.getPeopleFromCache(),
+			getPeopleFromNetworkTrigger(countPeople))
 			.doOnNext(listResponseHolder -> {
 				if (ResponseHolder.Source.NETWORK == listResponseHolder.getSource() && !listResponseHolder.hasError()) {
 					// TODO: there must be a better way of preventing data coming in the wrong order.
@@ -55,23 +46,10 @@ public class PeopleDataManager {
 				-> ResponseHolder.Source.STORAGE != listResponseHolder.getSource() || !mNetworkDataFetched);
 	}
 
-	private Observable<ResponseHolder<List<Person>>> getPeopleFromCache() {
-		RealmResponseWrapper<Person> realmResponseWrapper = new RealmResponseWrapper<>();
-		return realmResponseWrapper.wrap(mPeopleRealmFetcher.getAsyncObservable(mRealm));
-	}
-
-	private Observable<ResponseHolder<List<Person>>> getPeopleFromNetworkTrigger(int countPeople) {
-		return mNetworkFetchTrigger.startWith((Void) null).flatMap(aVoid -> getPeopleFromNetwork(countPeople));
-	}
-
-	private Observable<ResponseHolder<List<Person>>> getPeopleFromNetwork(int countPeople) {
-		RealmUpdateMethod<List<Person>> realmUpdateMethod = new ReplaceListRealmUpdateMethod<>(mPeopleRealmFetcher);
-		RealmUpdater<List<Person>> realmAsyncUpdater = new BasicAsyncRealmUpdater<>(mRealm, realmUpdateMethod);
-		mPeopleNetworkFetchAndUpdateList = new NetworkFetchAndUpdateList<>(
-			mPeopleService.getPeople(countPeople), realmAsyncUpdater);
-
-		NetworkResponseWrapper<Person> networkResponseWrapper = new NetworkResponseWrapper<>();
-		return networkResponseWrapper.wrap(mPeopleNetworkFetchAndUpdateList.getNetworkObservable());
+	public Observable<ResponseHolder<List<Person>>> getPeopleFromNetworkTrigger(int countPeople) {
+		return mNetworkFetchTrigger
+			.startWith((Void) null)
+			.flatMap(aVoid -> mPeopleDataLoader.getPeopleFromNetwork(countPeople));
 	}
 
 	public void triggerNetworkUpdate() {
